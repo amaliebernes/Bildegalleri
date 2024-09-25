19078171f871dc82eb33282c02aa321856a1a5bd
amaliebernes <bernesamalie@gmail.com>
2024-09-25 13:41:18 +0200

    Next button

--- script.js
+++ script.js
@@ -1,52 +1,60 @@
+//hvilket bilde er vi på nå
+
 let currentImage = 0;
 
 // Elementene vi trenger fra HTML
 
-let allImages = document.querySelectorAll (".carusell img");
+let allImages = document.querySelectorAll(".carusell img");
 
-console.log(allImages);
+function showImage(imageIndex) {}
 
- 
+function moveImage(offset) {
+  let newIndex = currentImage + offset;
+  let totalImages = allImages.length - 1;
 
+  // Hvilket bilde er vi på nå
 
+  // Regn ut hvilket bilde som blir neste
 
+  // Og oppdater nåværende bilde
 
+  if (newIndex < 0) {
+    // Viss vi prøver å gå under null
+    // gå til siste bildet
+    currentImage = totalImages;
+  } else if (newIndex > totalImages) {
+    // Viss vi prøver å gå til over antall bilder vi har
+    // gå til start bildet
+    currentImage = 0;
+  } else {
+    // Viss vi vi har bildet
+    // gå til bildet
+    currentImage = newIndex;
+  }
 
-function showImage (imageIndex) {
+  //for hvert bilde element kjør denne koden
+
+  //
+  allImages.forEach((image, index) => {
+    if (index === currentImage) {
+      image.className = "current-image";
+    } else {
+      image.className = "";
+    }
+  });
 }
 
-function moveImage (offset) {
+// Fjern curret-image klassen fra nåværende bilde
 
+// Legg til current-image på det nye bildet
 
-    // Hvilket bilde er vi på nå 
+let previousButton = document.querySelector("#previouse-button");
+let nextButton = document.querySelector("#next-button");
 
+previousButton.addEventListener("click", () => {
+  moveImage(-1);
+});
 
-
-    // Regn ut hvilket bilde som blir neste
-
-let newImageIndex = currentImage + offset
-
-allImages.forEach {(image, index) =>
-
-if (index === newImageIndex)  {
-
-    image.className = "current-image"
-}
-
-else {
-image.className ="";
-
-}
-
-}
-
-    // Fjern curret-image klassen fra nåværende bilde
-
-    // Legg til current-image på det nye bildet
-
-};
-console. log(allImages); 
-
-moveImage(1);
-
-console. log (allImages);
+nextButton.addEventListener("click", () => {
+  moveImage(1);
+});
